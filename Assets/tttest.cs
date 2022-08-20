7b8d57bc54d504d7a9397b31efcf3497e331c265
unarch <unarch@outlook.com>
2022-08-21 00:11:49 +0800

    初始化项目

---
+++ Assets/tttest.cs
@@ -0,0 +1,28 @@
+using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tttest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hello");
+        print("wow!");
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("hello world!");
+        var rtf = gameObject.GetComponent<RectTransform>();
+        var brn = gameObject.GetComponent<Canvas>();
+
+        
+    }
+
+}
